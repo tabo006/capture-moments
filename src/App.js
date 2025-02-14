0d9d8c926419d4caed7abad1ff585197a0940a34
tabo006 <90430707+tabo006@users.noreply.github.com>
2025-02-13 23:27:31 -0500

    edited configs for publish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route,createBrowserRouter,createRoutesFromElements, RouterProvider, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Account from './pages/Account';  
 import Booking from './pages/Booking';
@@ -10,27 +10,19 @@ import Contact from './pages/Contact';
 import Studios from './pages/Studios';
 import Services from './pages/Services';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path="/" element={<Home />} />
-
-    <Route path='/booking' element={<Booking/>} />
-    <Route path='/services' element={<Services/>} />
-    <Route path='/confirmation' element={<Confirmation/>} />
-    <Route path='/contact' element={<Contact/>} />
-    <Route path='/studios' element={<Studios/>} />
-    <Route path='/account' element={<Account/>} />
-      </>
-
-  )
-);
-
 function App() {
   return (
-    <body>
-    <RouterProvider router={router} />
-    </body>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/studios" element={<Studios />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </Router>
   );
 }
 
